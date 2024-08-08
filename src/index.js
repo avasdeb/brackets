a8133ff0924400c70277fc956cb16e94263953de
avasdeb <avasdeb@gmail.com>
2024-08-08 23:15:01 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	// your solution
+	bracketsConfig = bracketsConfig.map(item => item.join(''));
+	while (bracketsConfig.length) {
+		let a = 0;
+		bracketsConfig.forEach(item => {
+			if (str.indexOf(item) != -1) {
+				str = str.replace(item, '');
+			}
+			else {
+				a += 1;
+			}
+		})
+		if (a === bracketsConfig.length) {
+			bracketsConfig.length = 0;
+		}
+	}
+	return str.length === 0 ? true : false;
 }
